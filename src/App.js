b6b7d43807c71c0895ed030478d184c5af24d46d
panxumin <joseypan@163.com>
2022-09-22 17:25:20 +0800

    feat: 添加输入表单组件

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -11,8 +12,7 @@ function App() {
   ];
   return (
     <div className="App">
-      <h2>Let's get started!</h2>
-      <p>This is also visible!</p>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
