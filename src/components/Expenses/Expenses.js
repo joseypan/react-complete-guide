aeb146fee1d338b68588232ab4924ca6674f7520
panxumin <joseypan@163.com>
2022-09-27 14:34:36 +0800

    feat: 新增ExpensesFilter组件，并完成切换逻辑

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,19 @@
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 import "./Expenses.css";
 import Card from "../UI/Card";
 function Expenses({ expenses }) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (year) => {
+    setFilteredYear(year);
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        filteredYear={filteredYear}
+        onFilterChange={filterChangeHandler}
+      />
       {expenses.map((item, index) => {
         return (
           <ExpenseItem
